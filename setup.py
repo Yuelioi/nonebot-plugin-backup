31ec5f591f8f201241cc3883c57dba61770d023e
yueloi <yuelioi1210@gmail.com>
2022-12-15 01:13:17 +0800

    update

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="nonebot-plugin-backup",
-    version="2.2.2",
-    author="Kl1nge5",
+    version="1.0.0",
+    author="Yueli",
     description="A plugin based on NoneBot2 to backup files in qq group.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ninthseason/nonebot-plugin-backup",
+    url="https://github.com/Yuelioi/nonebot-plugin-backup",
     project_urls={
-        "Bug Tracker": "https://github.com/ninthseason/nonebot-plugin-backup/issues",
+        "Bug Tracker": "https://github.com/Yuelioi/nonebot-plugin-backup/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    packages=["nonebot_plugin_backup"],
+    # packages=["nonebot_plugin_backup"],
     python_requires=">=3.7",
     install_requires=[
         "nonebot2 >= 2.0.0b2",
